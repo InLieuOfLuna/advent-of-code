7e67ea0d461cec020e857ff496751b257e4cf4d4
luna <lunagibson@duck.com>
2022-12-31 08:36:31 -0700

    Code cleanup

--- src/test/kotlin/adventofcode/year2022/CampCleanupTest.kt
+++ src/test/kotlin/adventofcode/year2022/CampCleanupTest.kt
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test
 
 import org.junit.jupiter.api.Assertions.*
 
-class CampCleanupTest: SolverTest(
+object CampCleanupTest: SolverTest(
     """
         2-4,6-8
         2-3,4-5
--- src/test/kotlin/adventofcode/year2022/RucksackReorganizationTest.kt
+++ src/test/kotlin/adventofcode/year2022/RucksackReorganizationTest.kt
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test
 
 import org.junit.jupiter.api.Assertions.*
 
-class RucksackReorganizationTest: SolverTest(
+object RucksackReorganizationTest: SolverTest(
     """
         vJrwpWtwJgWrhcsFMMfFFhFp
         jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
