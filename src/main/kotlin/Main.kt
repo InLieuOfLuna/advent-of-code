1221c804703f0b1015dcaa1dd824eaf551732daa
luna <lunagibson@duck.com>
2022-12-30 18:50:20 -0700

    added selection menu

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,24 @@
-import adventofcode.year2022.HillClimbingAlgorithm
+import adventofcode.year2022.*
 import java.util.Scanner
 
+val year2022 = listOf(
+    CalorieCounting,
+    RockPaperScissors,
+    RucksackReorganization,
+    CampCleanup,
+    HillClimbingAlgorithm
+)
+
 fun main() {
-    println(HillClimbingAlgorithm.part2(readLines()))
+    val year = year2022
+    print("Day [1-${year.size}]: ")
+    val day = year[readln().toInt() - 1]
+    print("Part 1 or 2: ")
+    val part = readln()
+    println("Paste input below:")
+    val input = readLines()
+    if (part == "1") println(day.part1(input))
+    if (part == "2") println(day.part2(input))
 }
 
 private fun readLines() = sequence {
